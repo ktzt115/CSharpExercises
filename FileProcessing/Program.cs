720a59fc74c073ebcf01dbab7091adc0ebef14cd
Khin Thuzar Thin <khin.thuzar.thin@avanade.com>
2023-09-04 03:00:46 +0800

    done 2-3 exercise

---
+++ FileProcessing/Program.cs
@@ -0,0 +1,49 @@
+﻿//Workbook 2, 2-24 , Exercise 1
+
+namespace FileProcessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var fileName = "PayrollData3.txt";
+                var fileFullPath = @$"C:\Users\khin.thuzar.thin\Downloads\KPayRolls\{fileName}";
+                if (File.Exists(fileFullPath))
+                {
+                    using (StreamReader reader = new StreamReader(fileFullPath))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            var data = reader.ReadLine();
+                            TimeCard tc = TimeCard.CreateTimeCard(data);
+                            Console.WriteLine($"GrossPay for {tc.Name} : {tc.GetGrossPay():N2}");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File Does not exists");
+                }
+            }
+            catch(InvalidCastException castException)
+            {
+                Console.WriteLine(castException.Message);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("File does not exists! Please provide correct file");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Some Exception happened.");
+            }
+            finally
+            {
+                Console.WriteLine("File Processing Complete");
+            }
+            
+        }
+    }
+}
---
+++ FileProcessing/TimeCard.cs
@@ -0,0 +1,33 @@
+﻿namespace FileProcessing
+{
+    public class TimeCard
+    {
+        public string Name { get; set; }
+        public int HoursWorked { get; set; }
+        public double PayRate { get; set; }
+
+        public static TimeCard CreateTimeCard(string inputToParse)
+        {
+            var splitted = inputToParse.Split('|');
+            if(splitted.Any() && splitted.Length >= 3)
+            {
+                return new TimeCard
+                {
+                    Name = splitted[0],
+                    HoursWorked = int.Parse(splitted[1]),
+                    PayRate = double.Parse(splitted[2])
+                };
+            }
+            else
+            {
+                throw new InvalidCastException("The data format provided is not valid one. Please modify the file.");
+            }
+            
+        }
+
+        public double GetGrossPay()
+        {
+            return HoursWorked > 40 ? HoursWorked * PayRate * 1.5 : HoursWorked * PayRate;
+        }
+    }
+}
