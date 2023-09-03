28246e70bd5ad7476c6073aa3af74ea12e7f208d
Khin Thuzar Thin <khin.thuzar.thin@avanade.com>
2023-09-04 00:33:44 +0800

    done module 2 exercies

--- MortageCalculator/Program.cs
+++ MortageCalculator/Program.cs
@@ -1,10 +1,29 @@
-﻿//MortageCalculator
+﻿//2-45 , 2 , MortageCalculator
 
-double amountBorrow = 53000.00;
-double interestRate = 7.625;
-int loanYears = 15;
+Console.WriteLine("How much are you borrowing? ");
+string input = Console.ReadLine(); 
+double amountBorrow = double.Parse(input);
+Console.WriteLine("What is your interest rate? ");
+input = Console.ReadLine();
+double interestRate = double.Parse(input)/100;
 
-double paymentAmount = 9;
-Console.WriteLine($"Your estimated payment is {paymentAmount: C}");
-Console.WriteLine();
-Console.WriteLine();
+Console.WriteLine("How long is your loan (in years)? ");
+input = Console.ReadLine();
+int loanYears = int.Parse(input);
+
+double monthlyInterestRate =  interestRate / 12;
+double numberOfMonthlyPayment = loanYears * 12;
+double paymentAmount = getPaymentAmount(amountBorrow, monthlyInterestRate, numberOfMonthlyPayment);
+double totalPayment = paymentAmount * numberOfMonthlyPayment;
+double totalInterestPaid = totalPayment - amountBorrow;
+
+Console.WriteLine($"Your estimated payment is {paymentAmount:F2}");
+Console.WriteLine($"You paid {totalPayment:C} over the life of the loan");
+Console.WriteLine($"Your total interest cost for the loan was {totalInterestPaid:C}");
+
+double getPaymentAmount(double amountBorrow, double monthlyInterestRate, double numberOfMonthlyPayment)
+{
+
+    return Math.Round(amountBorrow * monthlyInterestRate / (1 - Math.Pow(1 + monthlyInterestRate, -1 * numberOfMonthlyPayment )), 2, MidpointRounding.ToEven);
+
+}
--- TemperatureConversion/Program.cs
+++ TemperatureConversion/Program.cs
@@ -1,6 +1,10 @@
-﻿//excises 2-34 , 2
-double temperatureInFahrenheit = 32;
+﻿//excises 2-45 , 1 
 double multiplier = 5.0f / 9.0f;
-Console.WriteLine($"Temperature In Fahrenheit is {temperatureInFahrenheit}\u00B0F");
+
+Console.WriteLine("Enter the temperature in Fahrenheit - ");
+string input = Console.ReadLine();
+double temperatureInFahrenheit = double.Parse(input);
+
+Console.WriteLine($"Temperature In Fahrenheit is {temperatureInFahrenheit:F1}\u00B0F");
 double temperatureInCelsius = (temperatureInFahrenheit - 32) * multiplier;
-Console.WriteLine($"Temperature In Celsius is {temperatureInCelsius}\u00B0C");
+Console.WriteLine($"Temperature In Celsius is {temperatureInCelsius:F1}\u00B0C");
