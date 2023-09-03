0661c1faf98c54933203d36036a2f1037c636d9d
Khin Thuzar Thin <khin.thuzar.thin@avanade.com>
2023-09-03 23:35:19 +0800

    exercise 1 done

--- AddressLabel/Program.cs
+++ AddressLabel/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Khin Thuzar Thin";
+string streetAddress = "New Valley Street";
+string apartmentNumber = "123";
+string city = "Sious Falls";
+string state = "South Dakota";
+
+string addressLabel = $"{name}\n{streetAddress}, Apt #{apartmentNumber}\n{city},{state}";
+Console.WriteLine(addressLabel);
